ab81cd4a5364a1ab1ccc63841d18532e8b8511a8
nantha007 <nantha07@terpmail.umd.edu>
2019-11-28 20:31:18 -0500

    unpooling modifies

--- net/classifier1.py
+++ net/classifier1.py
@@ -38,6 +38,7 @@ class UpconvLayer(nn.Module):
     def __init__(self, a_in_channels , a_out_channels, a_kernel_size=5, a_stride=1):
         super(UpconvLayer, self).__init__()
         self.unpool = nn.UpsamplingNearest2d(scale_factor=2)
+        # self.unpool = self.zeropad
         self.conv = nn.Conv2d(a_in_channels, a_out_channels, kernel_size=a_kernel_size, stride=a_stride, padding=2)
         self.conv.weight.data.normal_(0.0, 0.01)
 
@@ -45,6 +46,11 @@ class UpconvLayer(nn.Module):
         a_hidden = self.unpool(a_input)
         a_out = self.conv(a_hidden)
         return a_out
+
+    def zeropad(self, x, stride=2):
+        w = x.new_zeros(stride, stride)
+        w[0, 0] = 1
+        return F.conv_transpose2d(x, w.expand(x.size(1), 1, stride, stride), stride=stride, groups=x.size(1))
 
 class DepthPredictionNet(nn.Module):
     """
@@ -69,7 +75,7 @@ class DepthPredictionNet(nn.Module):
         self.conv2 = nn.Conv2d(64, 1, kernel_size=3, stride=1, padding=1)
         self.dropout1 = nn.Dropout2d(p= 0.5, inplace=False)
         self.conv2.weight.data.normal_(0.0, 0.01)
-        self.upsample = nn.UpsamplingBilinear2d(size=(480, 640))
+        self.upsample = nn.UpsamplingBilinear2d(size=(228, 304))
 
     def forward(self, a_input):
         """
@@ -95,3 +101,49 @@ class DepthPredictionNet(nn.Module):
         l_output = self.upsample(l_output1)
         #print(l_output.shape)
         return l_output
+
+class interleavingMaps(nn.Module):
+    """
+    Upconvolution layer
+    3x3
+    2x3
+    3x2
+    2x2
+    interleave
+    """
+    def __init__(self, a_in_channels , a_out_channels, a_kernel_size=5, a_stride=1):
+        super(interleavingMaps, self).__init__()
+        # self.unpool = nn.UpsamplingNearest2d(scale_factor=2)
+        self.conv1 = nn.Conv2d(a_in_channels, a_out_channels, kernel_size=(3,3), stride=a_stride, padding=1)
+        self.conv2 = nn.Conv2d(a_in_channels, a_out_channels, kernel_size=(2,3), stride=a_stride)
+        self.conv3 = nn.Conv2d(a_in_channels, a_out_channels, kernel_size=(3,2), stride=a_stride)
+        self.conv4 = nn.Conv2d(a_in_channels, a_out_channels, kernel_size=(2,2), stride=a_stride)
+        # self.zeroPad1 = nn.
+        self.conv1.weight.data.normal_(0.0, 0.01)
+        self.conv2.weight.data.normal_(0.0, 0.01)
+        self.conv3.weight.data.normal_(0.0, 0.01)
+        self.conv4.weight.data.normal_(0.0, 0.01)
+
+    def forward(self, a_input):
+        map1 = self.conv1(a_input)
+        map2 = self.conv2(a_input)
+        map3 = self.conv3(a_input)
+        map4 = self.conv4(a_input)
+        a_out = self.interleave(map1, map2, map3, map4)
+        return a_out
+
+    def interleave(self, map1, map2, map3, map4):
+        shape_map = map1.shape[1]
+        print(map1.shape)
+        print(map2.shape)
+        inter_map1 = torch.stack((map1, map2), dim=0).view(shape_map, shape_map*2).t().continguous().view(shape_map, shape_map*2)
+        inter_map2 = torch.stack((map3, map4), dim=0).view(shape_map, shape_map*2).t().continguous().view(shape_map, shape_map*2)
+        final_unpool = torch.stack((inter_map1, inter_map2), dim=0).view(shape_map*2, shape_map*2).unsqueeze(dim = 0)
+        return final_unpool
+
+'''
+def pad_within(x, stride=2):
+   w = x.new_zeros(stride, stride)
+   w[0, 0] = 1
+   return F.conv_transpose2d(x, w.expand(x.size(1), 1, stride, stride), stride=stride, groups=x.size(1))
+'''
